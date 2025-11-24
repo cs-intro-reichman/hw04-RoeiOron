537a7aa923eb7d54e6bdf420733cf577b5ad3e79
Roei Oron <roeioron@gmail.com>
2025-11-24 19:33:07 +0200

    f

--- ArrCharOps.java
+++ ArrCharOps.java
@@ -84,13 +84,14 @@ public class ArrCharOps {
         if (n == 0) {
             return 0;
         }
+        
         long hashValue = 0;
         for (int i = 0; i < n; i++) {
             hashValue = hashValue * 7 + arr[i];
         }
         return hashValue;
     }
-
+    
     public static int compareTo(String str1, String str2) {
         int len1 = str1.length();
         int len2 = str2.length();
@@ -109,7 +110,7 @@ public class ArrCharOps {
                 }
             }
         }
-        
+    
         if (len1 < len2) {
             return -1;
         } else if (len1 > len2) {
